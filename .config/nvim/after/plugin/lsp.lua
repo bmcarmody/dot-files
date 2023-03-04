a0c34497bc434d00e40f6aed1ee2252c764aa5e5
Brandon Carmody <BmcPrimary@yahoo.com>
2023-03-04 12:32:39 -0700

    Add diagnostic icons to line numbers

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -10,8 +10,20 @@ lsp.ensure_installed({
   'eslint',
 })
 
+-- Diagnostic info
 vim.lsp.handlers["textDocument/publishDiagnostics"] =
 	vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = true })
+local signs = {
+    Error = " ",
+    Warn = " ",
+    Hint = " ",
+    Info = " "
+}
+
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = hl})
+end
 
 -- (Optional) Configure lua language server for neovim
 lsp.nvim_workspace()
