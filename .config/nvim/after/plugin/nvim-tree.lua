a729cc2237114e03af2fd9e69e218978c8af6ed2
Brandon Carmody <BmcPrimary@yahoo.com>
2023-03-26 09:46:11 -0600

    Make Nvim tree follow directory in buffer

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -1,5 +1,13 @@
 vim.opt.termguicolors = true
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+  update_cwd = true,
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+  }
+})
 
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>")
+
+vim.g.nvim_tree_respect_buf_cwd = 1
